261cbd5a2f32b8afb5ee431693fbceb708413091
Adrian Rangel Martinez <142278016+AdrianRang@users.noreply.github.com>
2025-03-07 17:45:39 -0600

    change score commands (check TODO)

--- src/main/java/frc/robot/commands/CompoundCommands/ScoringCommands.java
+++ src/main/java/frc/robot/commands/CompoundCommands/ScoringCommands.java
@@ -1,9 +1,8 @@
 package frc.robot.commands.CompoundCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.RobotState;
@@ -14,10 +13,11 @@ import frc.robot.subsystems.Elevator;
 
 public class ScoringCommands {
     public static Command scorePositionCommand(RobotState level, Elevator elevator, CoralIntakeArm arm, CoralIntakeWrist wrist) {
-        return new SequentialCommandGroup(
+        // TODO: Leyva dice que asi si jala, depracaria scorePositionAutoCommand y se usaria este para los 2, falta probarlo
+        return Commands.sequence(
             elevator.setSetpointCommand(level.getElevatorPosition()),
             arm.setSetpointCommand(level.getArmPosition()),
-            new WaitCommand(0.5),
+            Commands.waitSeconds(0.5),
             // * If it is for trough, make the wrist parallel
             wrist.setSetpointCommand(level.getWristPosition())
         );
