269e1148defde588b21d9f9d2960272360ce022d
Ramón de León <ramondeleonca@gmail.com>
2025-03-26 23:21:29 -0600

    Added state machine to scoring commands

--- src/main/java/frc/robot/commands/CompoundCommands/ScoringCommands.java
+++ src/main/java/frc/robot/commands/CompoundCommands/ScoringCommands.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.CompoundCommands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -13,9 +14,31 @@ import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.CoralIntakeArm.ArmPosition;
 
 public class ScoringCommands {
+    public static class StateMachine {
+        private static StateMachine instance = null;
+        private RobotState currentState = RobotState.HOME;
+
+        public StateMachine() {}
+
+        public static StateMachine getInstance() {
+            if (instance == null) instance = new StateMachine();
+            return instance;
+        }
+
+        public void setState(RobotState state) {
+            currentState = state;
+            SmartDashboard.putString("RobotState/StateLevel", state.toString());
+        }
+
+        public RobotState getState() {
+            return currentState;
+        }
+    }
+
     public static Command scorePositionCommand(RobotState level, Elevator elevator, CoralIntakeArm arm, CoralIntakeWrist wrist) {
         // TODO: Leyva dice que asi si jala, depracaria scorePositionAutoCommand y se usaria este para los 2, falta probarlo
         return Commands.sequence(
+            new InstantCommand(() -> StateMachine.getInstance().setState(level)),
             elevator.setSetpointCommand(level.getElevatorPosition()),
             arm.setSetpointCommand(level.getArmPosition()),
             Commands.waitSeconds(0.5),
@@ -25,10 +48,11 @@ public class ScoringCommands {
 
     public static Command scorePositionAutoCommand(RobotState level, Elevator elevator, CoralIntakeArm arm, CoralIntakeWrist wrist) {
         return new SequentialCommandGroup(
-            new InstantCommand(() -> arm.setSetpoint(level.getArmPosition())),
-            new InstantCommand(() -> elevator.setSetpoint(level.getElevatorPosition())),
+            new InstantCommand(() -> StateMachine.getInstance().setState(level)),
+            new InstantCommand(() -> arm.setSetpoint(level.getArmPosition()), arm),
+            new InstantCommand(() -> elevator.setSetpoint(level.getElevatorPosition()), elevator),
             new WaitCommand(0.5),
-            new InstantCommand(() -> wrist.setSetpoint(level.getWristPosition()))
+            new InstantCommand(() -> wrist.setSetpoint(level.getWristPosition()), wrist)
         );
     }
     
